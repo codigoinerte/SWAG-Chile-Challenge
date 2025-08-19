36d17224862dd45a9ffafb7265ff63d45bc631e8
codigoinerte <fmartinez.bpe@gmail.com>
2025-08-19 18:16:42 -0500

    feat: Implement price sorting from lowest to highest in product list

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -43,6 +43,8 @@ const ProductList = () => {
         break
       case 'price':
         // Price sorting to implement
+        /* price sorting from lowest to highest */
+        filtered.sort((a,b) => a.basePrice - b.basePrice)
         break
       case 'stock':
         filtered.sort((a, b) => b.stock - a.stock)
